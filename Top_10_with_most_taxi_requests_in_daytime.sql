524d44dc2a91a0bfbeeb8c99265e12a6211ba502
yal067 <yl4294@columbia.edu>
2024-11-29 22:49:37 -0500

    git commit -m<Update files>
    -Update sql.files
    -Fix errors in 'import'

--- Top_10_with_most_taxi_requests_in_daytime.sql
+++ Top_10_with_most_taxi_requests_in_daytime.sql
@@ -9,7 +9,8 @@ JOIN
 ON
     strftime('%Y-%m-%d', t.pickup_datetime) = s.DATE
 WHERE
-    strftime('%Y-%m-%d', t.pickup_datetime) BETWEEN '2020-01-01' AND '2023-12-31'
+    strftime('%Y-%m-%d', t.pickup_datetime) BETWEEN '2020-01-01'
+        AND '2023-12-31'
     AND strftime('%H', t.pickup_datetime) >= strftime('%H', s.Sunrise)
     AND strftime('%H', t.pickup_datetime) <= strftime('%H', s.Sunset)
 GROUP BY
--- schema.sql
+++ schema.sql
@@ -1,54 +1,54 @@
 
 Create Table IF NOT EXISTS hourly_weather (
-   id INTEGER PRIMARY KEY,
-   DATE DATETIME,
-   hour INTEGER,
-   HourlyPrecipitation REAL,
-   HourlyWindSpeed REAL
+    id INTEGER PRIMARY KEY,
+    DATE DATETIME,
+    hour INTEGER,
+    HourlyPrecipitation REAL,
+    HourlyWindSpeed REAL
 );
 
 Create Table IF NOT EXISTS daily_weather (
-   id INTEGER PRIMARY KEY,
-   DATE DATETIME,
-   DailyPrecipitation REAL,
-   DailyAverageWindSpeed REAL,
-   DailySnowfall REAL
+    id INTEGER PRIMARY KEY,
+    DATE DATETIME,
+    DailyPrecipitation REAL,
+    DailyAverageWindSpeed REAL,
+    DailySnowfall REAL
 );
 
 CREATE TABLE IF NOT EXISTS taxi_trips (
-   id INTEGER PRIMARY KEY,
-   pickup_datetime DATETIME,
-   dropoff_datetime DATETIME,
-   trip_distance REAL,
-   pickup_longitude REAL,
-   pickup_latitude REAL,
-   dropoff_longitude REAL,
-   dropoff_latitude REAL,
-   fare_amount REAL,
-   surcharge REAL,
-   taxes REAL,
-   tolls_amount REAL,
-   tip_amount REAL
+    id INTEGER PRIMARY KEY,
+    pickup_datetime DATETIME,
+    dropoff_datetime DATETIME,
+    trip_distance REAL,
+    pickup_longitude REAL,
+    pickup_latitude REAL,
+    dropoff_longitude REAL,
+    dropoff_latitude REAL,
+    fare_amount REAL,
+    surcharge REAL,
+    taxes REAL,
+    tolls_amount REAL,
+    tip_amount REAL
 );
 
 Create Table IF NOT EXISTS uber_trips (
-   id INTEGER PRIMARY KEY,
-   request_datetime DATETIME,
-   driver_arrived_datetime DATETIME,
-   pickup_datetime DATETIME,
-   dropoff_datetime DATETIME,
-   trip_miles REAL,
-   trip_time_seconds INTEGER,
-   pickup_longitude REAL,
-   pickup_latitude REAL,
-   dropoff_longitude REAL,
-   dropoff_latitude REAL,
-   base_passenger_fare REAL,
-   tolls REAL,
-   black_car_fund REAL,
-   sales_tax REAL,
-   congestion_surcharge REAL,
-   airport_fee REAL,
-   tips REAL,
-   all_surcharge REAL
+    id INTEGER PRIMARY KEY,
+    request_datetime DATETIME,
+    driver_arrived_datetime DATETIME,
+    pickup_datetime DATETIME,
+    dropoff_datetime DATETIME,
+    trip_miles REAL,
+    trip_time_seconds INTEGER,
+    pickup_longitude REAL,
+    pickup_latitude REAL,
+    dropoff_longitude REAL,
+    dropoff_latitude REAL,
+    base_passenger_fare REAL,
+    tolls REAL,
+    black_car_fund REAL,
+    sales_tax REAL,
+    congestion_surcharge REAL,
+    airport_fee REAL,
+    tips REAL,
+    all_surcharge REAL
 );
